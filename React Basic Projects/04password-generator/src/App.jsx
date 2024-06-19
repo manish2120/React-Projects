61bc51536d8b5234a1e2951381793d159de00870
Manish Chavan <manish.chavan113@gmail.com>
2024-06-19 20:48:38 +0530

    update styling

--- React Basic Projects/04password-generator/src/App.jsx
+++ React Basic Projects/04password-generator/src/App.jsx
@@ -1,89 +1,71 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
-// import './App.css'
+import './App.css'
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumber] = useState(false);
-  const [charAllowed, setCharacter] = useState(false);
+  const [charAllowed, setChar] = useState(false);
   const [password, setPassword] = useState('');
 
-  const passwordRef = useRef(null); //default value in parentheses
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = '';
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    let characters = "!@#$%^&*-_+=[]{}~`";
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
     if(numberAllowed) {
-      str += '0123465789';
+      str += '0123456789';
     }
 
     if(charAllowed) {
-      str += '!@#$%^&*-_+=[]{}~`';
+      str += '!@#$%^&*()~';
     }
 
-    for(let i = 0; i < length; i++) {
+    for(let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-
       pass += str.charAt(char);
     }
-
     setPassword(pass);
-  }, [length, numberAllowed, charAllowed, setPassword]);
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   useEffect(() => {
     passwordGenerator();
-  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
-  const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
+  const copyToClipboard = useCallback(() => {
     window.navigator.clipboard.writeText(password);
-  })
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 20);
+  }, [password]);
 
   return (
     <>
-     <div className='flex min-h-50 w-full justify-center px-3 py-2'>
-      <div className='bg-gray-700 px-5 py-5 rounded-2xl flex flex-col justify-center align-middle'>
-        <p className="text-center mb-2 font-bold">Password Generator</p>
-        <input type="text"
-        placeholder="password"
-        value={password}
-        className="rounded-md px-3 py-2 bg-white text-black"/>
-        <button onClick={copyPasswordToClipboard}>copy</button>
-        <div className='flex gap-6'>
-          <div className="flex align-middle gap-2">
-            <p>{length}</p>
-          <input type="range" 
-          id="passLength"
-          value={length}
-          min={8}
-          max={20}
-          ref={passwordRef}
-          onChange={(e) => {setLength(e.target.value)}}/>
-          <label htmlFor="passLength">Length</label>
-          </div>
-
-          <div className="flex align-middle gap-1">
-          <input type="checkbox" id="number" onChange={() => {
-            setNumber((prev) => !prev)
-          }}/>
-          <label htmlFor="number">Numbers</label>
-          </div>
-          
-          <div className="flex align-middle gap-1">
-          <input type="checkbox" id="character" value={length} onChange={() => {
-            setCharacter((prevNum) => !prevNum)
-          }}/>
-          <label htmlFor="character">Characters</label>
+      <div className='w-full h-screen flex items-center font-poppins'>
+        <div className='max-w-lg min-w-fit shadow-sm px-9 py-3 bg-gray-600 rounded-xl mx-auto'>
+          <h3 className='text-white text-xl font-semibold'>Password Generator</h3>
+          <div className='py-4 flex justify-center'>
+              <input type="text" placeholder="Password" value={password} className="cursor-pointer outline-none rounded-s-lg py-2 px-3 w-full" ref={passwordRef} readOnly/>
+              <button className="bg-blue-600 px-3 py-2 rounded-e-lg outline-none text-white font-bold" onClick={copyToClipboard}>copy</button>
+            </div>
+          <div className='text-white flex justify-center items-center flex-wrap py-3'>
+            <div className='flex items-center'>
+              <span>Length : </span>
+              <input type="range" min="0" max="20" value={length} onChange={(e) => setLength(e.target.value)} className="cursor-pointer mx-1"/>
+              <span>{length}</span>
+            </div>
+            <div className='flex items-center mx-4'>
+              <input type="checkbox" defaultChecked={numberAllowed} onChange={() => setNumber((prevNumb) => !prevNumb)} className="cursor-pointer mx-1"/>
+              <span>Numbers</span>
+            </div>
+            <div className='flex items-center'>
+              <input type="checkbox" defaultChecked={charAllowed} onChange={() => setChar((prevChar) => !prevChar)} className="cursor-pointer mx-1"/>
+              <span>Characters</span>
+            </div>
           </div>
         </div>
       </div>
-     </div>
     </>
   )
 }
 
 export default App
-
-// useCallback is used to cache the data in memory, it helps to optimize the code.
-// useEffect is used to 
-// useRef is used to get the reference
