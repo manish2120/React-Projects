c94c83746f0c85779b7839a916f07b51aa286223
Manish Chavan <manish.chavan113@gmail.com>
2024-06-19 20:35:41 +0530

    update styling

--- React Basic Projects/03bgColorChanger/src/App.jsx
+++ React Basic Projects/03bgColorChanger/src/App.jsx
@@ -1,29 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react"
+
 function App() {
-  const [color, setColor] = useState('#212121');
-  
+  const [color, setColor] = useState('#402E7A');
+
   return (
     <>
-    <div className='w-full h-screen duration-200' style={{backgroundColor: color, color: '#fff'}}>
-      <div className="flex justify-center align-middle">
-        <p className="text-xl font-bold mt-12">Tap to Change Background Color</p>
-        </div>
-        <div className='flex justify-center mt-7'>
-          <div className="fixed bottom-12 px-3 py-3 rounded-2xl bg-white flex flex-wrap justify-center gap-4">
-            <button className="px-5 py-2 rounded-2xl" style={{backgroundColor: "red"}} onClick={() => setColor('red')}>Red</button>
-            <button className="px-5 py-2 rounded-2xl" style={{backgroundColor: "blue"}}
-            onClick={() => setColor('blue')}>Blue</button>
-            <button className="px-5 py-2 rounded-2xl" style={{backgroundColor: "violet"}}
-            onClick={() => setColor('violet')}>Violet</button>
-            <button className="px-5 py-2 rounded-2xl" style={{backgroundColor: "yellow"}}
-            onClick={() => setColor('yellow')}>Yellow</button>
-            <button className="px-5 py-2 rounded-2xl"style={{backgroundColor: "brown"}}
-            onClick={() => setColor('brown')}>Brown</button>
-            <button className="px-5 py-2 rounded-2xl" style={{backgroundColor: "orange"}}
-            onClick={() => setColor('orange')}>Orange</button>
+    <div className="w-full h-screen duration-150 font-poppins" style={{backgroundColor: color}}>
+      <div className="w-full h-screen grid place-items-center text-white text-xl">
+        <p>Click on the buttons to change Background Color</p>
+      </div>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0">
+        <div className="flex flex-wrap justify-center gap-2 shadow-sm p-4 bg-gray-200 rounded-full">
+        <button style={{backgroundColor: 'red'}} onClick={() => setColor('red')} className="px-5 py-1 rounded-full shadow-md text-white font-semibold">Red</button>
+        <button style={{backgroundColor: 'green'}} onClick={() => setColor('green')} className="px-5 py-1 rounded-full shadow-md text-white font-semibold">Green</button>
+        <button style={{backgroundColor: 'blue'}} onClick={() => setColor('Blue')} className="px-5 py-1 rounded-full shadow-md text-white font-semibold">Blue</button>
+        <button style={{backgroundColor: 'violet'}} onClick={() => setColor('violet')} className="px-5 py-1 rounded-full shadow-md text-white font-semibold">Violet</button>
+        <button style={{backgroundColor: 'brown'}} onClick={() => setColor('brown')} className="px-5 py-1 rounded-full shadow-md text-white font-semibold">Brown</button>
           </div>
         </div>
-        </div>
+    </div>
     </>
   )
 }
